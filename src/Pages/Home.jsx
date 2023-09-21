2ae0e25a9b38688229d458b8fe15518de69494f6
Taejin Kim <rlaxowls1316@naver.com>
2023-09-21 21:03:19 +0900

    메인페이지 꾸미기

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 import { movies } from "../util/test_data";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Home() {
   return (
     <Container>
       <MoviewPoster>
-        {movies.results.map((movie) => {
-          return (
-            // <ul key={movie.id}>
-            <li key={movie.id}>{movie.title}</li>
-            // </ul>
-          );
-        })}
+        <Carousel
+          showThumbs
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          transitionTime={300}
+        >
+          {movies.results.map((movie) => {
+            return (
+              <ul key={movie.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                />
+                <li>{movie.title}</li>
+              </ul>
+            );
+          })}
+        </Carousel>
       </MoviewPoster>
     </Container>
   );
