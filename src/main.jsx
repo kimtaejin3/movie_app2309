aa120fb9b183650d02d5fbc57cada50c94359092
Taejin Kim <rlaxowls1316@naver.com>
2023-09-21 20:32:24 +0900

    전역 스타일 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "styled-components";
-// import GlobalStyles from "./styles/global-styles.js";
+import { GlobalStyles } from "./styles/global-styles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={{}}>
       <App />
-      {/* <GlobalStyles /> */}
+      <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>
 );
