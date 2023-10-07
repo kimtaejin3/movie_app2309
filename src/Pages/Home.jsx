52db733a21a7db7aec9179f391efbb8a1a457a22
Taejin Kim <rlaxowls1316@naver.com>
2023-10-07 16:01:52 +0900

    홈 api 연동

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,22 @@ import { movies } from "../util/test_data";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import PosterItem from "../Components/PosterItem";
+import { useState, useEffect } from "react";
+import { config } from "../../constant";
 
 export default function Home() {
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&api_key=" +
+        config.API_KEY
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setPopularMovies(data.results);
+      });
+  }, []);
+
   return (
     <Container>
       <MoviePoster>
@@ -15,7 +29,7 @@ export default function Home() {
           showStatus={false}
           transitionTime={300}
         >
-          {movies.results.map((movie) => {
+          {popularMovies.map((movie) => {
             return <PosterItem key={movie.id} movie={movie} />;
           })}
         </Carousel>
@@ -25,7 +39,5 @@ export default function Home() {
 }
 
 const Container = styled.div``;
-const MoviePoster = styled.div`
-  display: flex;
-  align-item: center;
-`;
+const MoviePoster = styled.div``;
+// display:flex; align-items:center;
